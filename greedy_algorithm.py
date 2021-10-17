b1c3c31d0cb1821fc103481d9c20561fea7dcd47
Jung-Min Ha <jmgoogling@gmail.com>
2021-10-17 18:25:29 +0900

    first commit

--- greedy_algorithm.py
+++ greedy_algorithm.py
@@ -26,3 +26,36 @@ print(greedy2(9830))
 # Coin 50: 0
 # Coin 10: 3
 # Total: 25
+
+# Make any number to 1 by minimum number of attempt - 1st attempt
+n, k = map(int, input().split()) # n must be bigger than k
+count = 0
+while True:
+    if n == 1:
+        break
+    if n % k == 0:
+        n /= k 
+        count += 1
+    else: 
+        n -= 1
+        count += 1
+print(count)
+    
+# Make any number to 1 by minimum number of attempt - 2nd attempt (Book)
+n, k = map(int, input().split())
+count = 0
+# Unlimited division of n by k under the condition of n >= k 
+while n >= k:
+    while n % k != 0:
+        n -= 1
+        count += 1
+    n //= k
+    count += 1
+# Substract 1 again
+while n > 1:
+    n -= 1
+    count += 1
+print(count)
+    
+     
+    
