674f0a06c01717e094f5226e88eb779fd0934cd0
Jung-Min Ha <67835149+hjmthebest@users.noreply.github.com>
2021-10-18 14:57:08 +0900

    Update greedy_algorithm_1.py

--- greedy_algorithm_1.py
+++ greedy_algorithm_1.py
@@ -1,4 +1,6 @@
-# Greedy Algorithm 1 - how much should I return change in order to save the number of coins maximum?
+# ====================================================================================================
+# Greedy Algorithm 1
+# How much should I return change in order to save the number of coins maximum?
 def greedy(money): # 9380
     coin = [500, 100, 50, 10]
     count = 0
@@ -28,7 +30,7 @@ print(greedy2(9830))
 # Total: 25
 
 # ====================================================================================================
-
+# Greedy Algorithm 2 
 # Make any number to 1 by minimum number of attempt - 1st attempt
 n, k = map(int, input().split()) # n must be bigger than k
 count = 0
@@ -47,14 +49,29 @@ print(count)
 n, k = map(int, input().split())
 count = 0
 # Unlimited division of n by k under the condition of n >= k 
-while n >= k: # 1st while clause starts here  
-    while n % k != 0: # 2nd while clause whether n is not divisible to k
-        n -= 1 # if so, we diminish 1 from n
-        count += 1 # and count that action
-    n //= k # [IMPORTANT] if n is divisible to k, it doesn't enter the 2nd while clause, but divide n by k
-    count += 1 # and count that action as well
+while n >= k:                                           # 1st while clause starts here  
+    while n % k != 0:                                   # 2nd while clause whether n is not divisible to k
+        n -= 1                                          # if so, we diminish 1 from n
+        count += 1                                      # and count that action
+    n //= k                                             # [IMPORTANT] if n is divisible to k, it doesn't enter the 2nd while clause, but divide n by k
+    count += 1                                          # and count that action as well
 # Substract 1 again
-while n > 1: # while n is superior to 1
-    n -= 1 # we diminish 1 from n
-    count += 1 # and it is also counted
-print(count) # final result
+while n > 1:                                            # while n is superior to 1
+    n -= 1                                              # we diminish 1 from n
+    count += 1                                          # and it is also counted
+print(count)                                            # final result
+
+# Make any number to 1 by minimum number of attempt - 3rd attempt (Me)
+n, k = map(int, input().split())
+count = 0
+while n >= k:
+    if n % k != 0:
+        n -= 1
+        count += 1
+    else:
+        n //= k
+        count += 1
+while n > 1:
+    n -= 1
+    count += 1
+print(count)
